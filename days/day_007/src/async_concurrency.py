d115e584034b5dbfd6c9646162c5e0caa373c283
Bruno Silva <silvamachadobruno@gmail.com>
2021-11-12 21:43:48 -0300

    Create async_concurrency.py

---
+++ days/day_007/src/async_concurrency.py
@@ -0,0 +1,30 @@
+import asyncio
+
+async def say(string_to_print:str, time_to_sleep:int):
+    await asyncio.sleep(time_to_sleep)
+    print(string_to_print)
+    
+async def sequence_saying():
+    await say('hello', 2)
+    await say('world', 3)
+    print("starting...")
+    
+    
+async def main():
+    await sequence_saying()
+    L = await asyncio.gather(
+        say("Will be the last print", 3),
+        say("Will be the second print", 2),
+        say("Will be the first print", 1),
+    )
+asyncio.run(main())
+
+# expected output:
+# hello
+# world
+# starting...
+# Will be the first print
+# Will be the second print
+# Will be the last print
+
+
