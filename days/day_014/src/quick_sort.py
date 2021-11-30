4464b1914f5595202f02bb5cda518f425a2d7564
Bruno Silva <silvamachadobruno@gmail.com>
2021-11-30 13:02:23 -0300

    Update quick_sort.py

--- days/day_014/src/quick_sort.py
+++ days/day_014/src/quick_sort.py
@@ -7,13 +7,14 @@ def quick_sort(array:list):
     number = array[pivot_index]
     smallers = [n for n in array if n < number]
     biggers = [n for n in array if n > number]
+    equals = [n for n in array if n == number]
     result = quick_sort(smallers)
-    result.append(number)
+    result.extend(equals)
     result.extend(quick_sort(biggers))
     return result
 
+test = quick_sort([1,5,2,3,3,6])
+assert[1,2,3,3,5,6] == test, 'test: "%s"' % test
 
-
-assert [1,2,3,4,5] == quick_sort([5,2,3,4,1])
-assert [1,2,3,4,5,6,7,8,9,10,11] == quick_sort([5,2,3,4,1,11,9,8,7,10,6])
-
+test = quick_sort([1,5,2,3,4,6])
+assert[1,2,3,4,5,6] == test, 'test: "%s"' % test
