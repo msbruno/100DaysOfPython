956760df79c4fde0fd926621e32e66ac8c9f55c4
Bruno Silva <silvamachadobruno@gmail.com>
2021-12-03 21:47:37 -0300

    Update lambda.py

--- days/day_023/src/lambda.py
+++ days/day_023/src/lambda.py
@@ -1,4 +1,4 @@
-# lambda expressions
+#lambda expressions
 
 def filter_even(arr:list):
    for element in arr:
@@ -8,16 +8,18 @@ def filter_even(arr:list):
 def filter_even_with_list_comprehension(arr:list):
    return [ element for element in arr if element % 2 == 0]
 
+
 filter_even_lambda = lambda arr: [element for element in arr if element % 2 == 0]
 
 test_data = [1,2,3,4,5,6]
 expect_result = [2,4,6]
+unexpected_result_text = lambda result: "result was:{}".format(result)
 
 result = list(filter_even(test_data))
-assert expect_result == result, "result was:{}".format(result)
+assert expect_result == result, unexpected_result_text(result)
 
 result = filter_even_with_list_comprehension(test_data)
-assert expect_result == result, "result was:{}".format(result)
+assert expect_result == result, unexpected_result_text(result)
 
 result = filter_even_lambda(test_data)
-assert expect_result == result, "result was:{}".format(result)
+assert expect_result == result, unexpected_result_text(result)
