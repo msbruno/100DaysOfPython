f66d007d8999b792780b25d4c58bd552789aacc9
Bruno Silva <silvamachadobruno@gmail.com>
2021-12-11 16:30:14 -0300

    Update queue.py

--- days/day_027/src/queue.py
+++ days/day_027/src/queue.py
@@ -1,6 +1,7 @@
 from collections import deque
 queue = deque(['1','2','3'])
 queue += ['4','5','6']
+
 queue.appendleft(7)
 print(queue)
 #deque(['7', '1', '2', '3', '4', '5', '6'])
@@ -13,6 +14,6 @@ queue.append(7)
 print(queue)
 #deque(['1', '2', '3', '4', '5', '6', '7'])
 
-queue.popleft()
+queue.pop()
 print(queue)
 #deque(['1', '2', '3', '4', '5', '6'])
