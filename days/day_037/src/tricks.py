bf62f8bf55de168be8156f8f2bc54db8ad852a95
Bruno Silva <silvamachadobruno@gmail.com>
2021-12-24 19:21:47 -0300

    Create tricks.py

---
+++ days/day_037/src/tricks.py
@@ -0,0 +1,26 @@
+
+# swipe
+x, y = 10, 20
+print(x, y)
+# 10 20
+
+x, y = y, x
+print(x, y)
+# 20 10
+
+# string reverse
+a = "ABCDEFGH"
+print("Reverse is", a[::-1])
+# HGFEDCBA
+
+
+#chainning comparisions
+n = 100
+result = 1 < n < 200
+print(result)
+# True
+
+result = 1 > n <= 99
+print(result)
+# False
+
