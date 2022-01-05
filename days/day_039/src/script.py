0438f0ffafcf672029871b178027ddf2c0cd1289
Bruno Silva <silvamachadobruno@gmail.com>
2022-01-05 18:01:12 -0300

    Create script.py

---
+++ days/day_039/src/script.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+count = 0
+@app.route("/")
+def hello():
+    count += 1
+    return "Executed {} times".format(count)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
